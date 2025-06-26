e5c4e9edeefdf3ded3f4473da97ef7a2f5f909b7
unknown <sitthipon.me@kkumail.com>
2025-06-26 14:51:13 +0700

    first commit

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'vuetify-materio',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'vuetify-materio',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
